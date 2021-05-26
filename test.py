4b91e5eeaead0712af544326f736d6b4ef4de10e
Aaron Hu <aaron@ip-192-168-1-18.us-west-2.compute.internal>
2021-05-26 12:52:53 -0700

    init commit

---
+++ test.py
@@ -0,0 +1,40 @@
+import requests
+import csv
+
+first_api_key = "OalyOEk3YNr2_NpcBcVD9662L8fTyJ8-Wrpdnzde4Eg"
+second_api_key = "OalyOEk3YNr2_NpcBcVD9662L8fTyJ8-Wrpdnzde4Eg"
+
+
+file_id = "1499142"
+
+response = requests.get('https://api.affinity.co/entity-files/' + file_id, auth=('', first_api_key))
+print("************** Download File ***************")
+old_org_id = str(response.json()['organization_id'])
+print(str(response.status_code) + " - download file.")
+file_name = response.json()['name']
+print(file_name)
+old_file_content = response.content
+old_file_header = response.headers['content-type']
+print("")
+
+print("************** GET old org info ***************")
+response = requests.get('https://api.affinity.co/organizations/' + old_org_id, auth=('', first_api_key))
+old_org_domain = response.json()['domain']
+print(str(response.status_code) + " - old org request.")
+print("")
+
+
+print("************** GET new org info ***************")
+response = requests.get('https://api.affinity.co/organizations?term=' + old_org_domain, auth=('', first_api_key))
+print(str(response.status_code) + " - new org request.")
+new_org_id = str(response.json()['organizations'][0]['id'])
+
+
+
+print("************** Upload file ***************")
+response = requests.post('https://api.affinity.co/entity-files', auth=('',second_api_key), params=({'organization_id': new_org_id}), files={'file': (file_name, old_file_content, old_file_header)})
+print(str(response.status_code) + " - upload status code.")
+print(response.json())
+
+
+
